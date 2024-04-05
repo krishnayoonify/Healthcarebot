0406411bc7fdb7a9baab6025d1dc8c19d919f6cd
krishna@yoonify.io <krishnayoonify>
2024-04-05 17:37:22 +0530

    local

--- healthcarebotbackend/healthcarebotbackend/settings.py
+++ healthcarebotbackend/healthcarebotbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-g*k18tv5l$-@z+ah*3qhnxttj5i_fka!%epzb+kds+s&xs&u6*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
